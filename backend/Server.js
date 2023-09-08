624c83cb90e81f3e9f8933ad41e1bf20a1e862de
LBS <linoybs720@gmail.com>
2023-09-08 16:11:06 +0300

    try to insert connection to mongo

--- backend/Server.js
+++ backend/Server.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const UploadRoute = require("./routes/UploadRoute");
 const app = express();
-const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const fileRoutes = require("./routes/fileRoutes");
-const molluskRoutes = require("./routes/molluskRoutes");
+const PORT = process.env.PORT || 5006;
 
-app.use("/v1/file", fileRoutes);
-app.use("/v1/mollusk", molluskRoutes);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+mongoose.connect(process.env.MONGO_URI, () => {
+   console.log("mongo conected")
 });
+
+app.use(UploadRoute);
+
+app.listen(PORT, () => {
+  console.log('server in ${PORT}')
+})
