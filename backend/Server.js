aecc48a538e960625edcd3dd7fff045d64b4884a
LBS <linoybs720@gmail.com>
2023-09-13 20:50:36 +0300

    fix get route!!! this is works

--- backend/Server.js
+++ backend/Server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); 
 const uploadRoute = require("./routes/UploadRoute");
 const getImageRoute = require("./routes/GetImageRoute");
-const connectToDatabase = require("./db");
+const connectToDatabase = require("./database/dbConnection");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 app.use("/v1", uploadRoute);
 app.use("/v1", getImageRoute);
-
 
 connectToDatabase();
 
--- backend/routes/GetImageRoute.js
+++ backend/routes/GetImageRoute.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const UploadModel = require("../models/UploadModel");
 
 router.get("/:fileId", async (req, res) => {
   try {
     const fileId = req.params.fileId; 
-
     const fileContent = await UploadModel.findOne({
       id: `${fileId}`,
     });
-
-    console.log(fileId);
-    console.log(fileContent);
+    const retentionTime = new Date(fileContent.retentionTime);
+    const fileExtension = fileContent.name.slice((fileContent.name.lastIndexOf(".") - 1 >>> 0) + 2);
+    console.log(fileContent.id + "." + fileExtension);
+    const imagePath = path.join(__dirname, "../public/uploads", fileContent.id + "." + fileExtension);
 
     if (!fileContent) {
       return res.status(404).json({ error: "File not found" });
     }
 
-    const retentionTime = new Date(fileContent.retentionTime);
-
     if (retentionTime <= new Date()) {
       return res.status(404).json({ error: "File has expired" });
     }
+    
+    res.sendFile(imagePath);
 
-    res.status(200).json({
-      id: fileContent.id,
-      name: fileContent.name,
-      retentionTime: fileContent.retentionTime,
-    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
